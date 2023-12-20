22d85a167f90c022e89bbe2aa953ae4cbe84a216
Menzo <canozmen@mail.com>
2023-12-20 16:25:24 -0700

    Update secretSanta.py

--- secretSanta.py
+++ secretSanta.py
@@ -1,16 +1,28 @@
 import random
-import time
 
 def clear_screen():
     # Print 50 empty lines to simulate clearing the screen
     print('\n' * 50)
 
-def secret_santa(participants):
-    # Check if the number of participants is valid
-    if len(participants) < 2 or len(participants) > 4:
-        print("Error: Number of participants should be between 2 and 4.")
-        return
+def get_participants():
+    while True:
+        try:
+            num_participants = int(input("Enter the number of participants (even number): "))
+            if num_participants % 2 == 0 and num_participants >= 2:
+                break
+            else:
+                print("Error: Please enter an even number greater than or equal to 2.")
+        except ValueError:
+            print("Error: Please enter a valid number.")
 
+    participants = []
+    for i in range(1, num_participants + 1):
+        name = input(f"{i}- Enter participant name: ")
+        participants.append(name)
+
+    return participants
+
+def secret_santa(participants):
     # Shuffle the participants to randomize the assignment
     random.shuffle(participants)
 
@@ -29,6 +41,8 @@ def secret_santa(participants):
         input("Press Enter to continue...")
         clear_screen()
 
-# Example usage with 4 participants
-participants_list = ["Alice", "Bob", "Charlie", "David"]
+# Get participants from user input
+participants_list = get_participants()
+
+# Run Secret Santa algorithm
 secret_santa(participants_list)
